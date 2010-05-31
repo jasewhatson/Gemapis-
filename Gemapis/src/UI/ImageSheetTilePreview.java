e804377c86e60a2d643dad29ef3f35a42e3b598a
peterpooter <peterpooter@34d2cac7-2255-7c49-7a0e-f21af0dea8bb>
2010-05-31 08:11:28 +0000

    General tidy of ImageSheetTilePreview. Bit more robust, though not great.

--- Gemapis/src/UI/ImageSheetTilePreview.java
+++ Gemapis/src/UI/ImageSheetTilePreview.java
@@ -2,12 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package UI;
 
-import MapEncodingDecoding.TileDataManagment.TileCanvas;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
@@ -28,8 +30,9 @@ import javax.swing.JTextField;
  * @author jason
  */
 public class ImageSheetTilePreview extends JPanel implements
-        java.awt.event.MouseMotionListener, PropertyChangeListener{
-    
+        java.awt.event.MouseMotionListener, PropertyChangeListener {
+
+    private ImageCanvas tileCanvas;
     private JScrollPane imageSheetScroller;
     private JPanel imageSheetPanel;
     private JPanel belowImageSheetPanel;
@@ -38,113 +41,136 @@ public class ImageSheetTilePreview extends JPanel implements
     private JTextField tileWidth;
     private JTextField tileHeight;
     private BufferedImage tileImage = null;
-    
-    //the last images that were added to the panels. Used for removing them
-    //  later ;)
-    private JLabel lastImageSheet = null;
-    private JLabel lastTilePreview = null;
+    private JLabel imageSheet = null;
     // floor(pos/get.width)* get.width
+    private String[] supportedFormats = new String[] {".jpg", ".jpeg", ".gif", ".png"};
+
     public ImageSheetTilePreview() {
-        
-        this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-        
+        this.setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
+
         imageSheetPanel = new JPanel();
-        
+        imageSheet = new JLabel();
+        imageSheetPanel.add( imageSheet );
         //imageSheetPanel.addMouseListener(this);
-        imageSheetPanel.addMouseMotionListener(this);
-        
-        imageSheetScroller = new JScrollPane(imageSheetPanel,
+        imageSheet.addMouseMotionListener( this );
+        imageSheetScroller = new JScrollPane( imageSheetPanel,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        
-        belowImageSheetPanel = new JPanel(new GridLayout(1,2));
-        
-        this.add(imageSheetScroller);
-        this.add(belowImageSheetPanel);
-        
-        imageSheetScroller.setPreferredSize(new Dimension(350, 50));
-        
-        dimensionsPanel = new JPanel(new GridLayout(2,2));
-        
-        tilePreviewPanel = new JPanel();
-        
-        belowImageSheetPanel.add(dimensionsPanel);
-        belowImageSheetPanel.add(tilePreviewPanel);
-        
-        dimensionsPanel.add(new javax.swing.JLabel("Tile width:"));
-        dimensionsPanel.add(tileWidth = new JTextField("32"));
-        dimensionsPanel.add(new javax.swing.JLabel("Tile height:"));
-        dimensionsPanel.add(tileHeight = new JTextField("60"));
-        
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED );
+
+        belowImageSheetPanel = new JPanel( new GridLayout( 1, 2 ) );
+        imageSheetScroller.setPreferredSize( new Dimension( 350, 150 ) );
+
+        dimensionsPanel = new JPanel( new GridLayout( 2, 2 ) );
+
+        tilePreviewPanel = new JPanel( new BorderLayout() );
+        //tilePreview = new JLabel();
+        tileCanvas = new ImageCanvas();
+        tilePreviewPanel.add( tileCanvas );
+
+
+
+        dimensionsPanel.add( new javax.swing.JLabel( "Tile width:" ) );
+        dimensionsPanel.add( tileWidth = new JTextField( "32" ) );
+        dimensionsPanel.add( new javax.swing.JLabel( "Tile height:" ) );
+        dimensionsPanel.add( tileHeight = new JTextField( "60" ) );
+
+        belowImageSheetPanel.add( dimensionsPanel );
+        belowImageSheetPanel.add( tilePreviewPanel );
+
+        this.add( imageSheetScroller );
+        this.add( belowImageSheetPanel );
     }
-    
-    public void propertyChange(PropertyChangeEvent evt) {
-        
+
+    private boolean isSupportedImage( File file ) {
+        String name = file.getAbsolutePath().toLowerCase();
+        for ( String str : supportedFormats ) {
+            if ( name.endsWith( str ) ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void propertyChange( PropertyChangeEvent evt ) {
+
         //Only listen to property changed evens
-        if (evt.getPropertyName().equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)) {
+        if ( evt.getPropertyName().equals( JFileChooser.SELECTED_FILE_CHANGED_PROPERTY ) ) {
             //Retrive the selected file
             File selectedFile = (File) evt.getNewValue();
-            
-            //If the file
-            if (selectedFile == null){
+
+            //If the file is null
+            if ( selectedFile == null ) {
                 return;
             }
-            
+
             //If the images is not null and is a image type file
-            if (
-                    selectedFile.getAbsolutePath().toLowerCase().endsWith(".jpg") ||
-                    selectedFile.getAbsolutePath().toLowerCase().endsWith(".jpeg") ||
-                    selectedFile.getAbsolutePath().toLowerCase().endsWith(".gif") ||
-                    selectedFile.getAbsolutePath().toLowerCase().endsWith(".png")) {
+            if ( selectedFile != null && isSupportedImage( selectedFile ) ) {
                 try {
-                    
-                    
-                    tileImage = ImageIO.read( new File(selectedFile.getAbsolutePath()) );
-                    
-                } catch (IOException ex) {
+                    tileImage = ImageIO.read( new File( selectedFile.getAbsolutePath() ) );
+
+                    imageSheet.setIcon( new ImageIcon( tileImage ) );
+                    int width = Integer.parseInt( this.tileWidth.getText() );
+                    int height = Integer.parseInt( this.tileHeight.getText() );
+                    setTileFromPreview( 0, 0, width, height );
+
+                    tilePreviewPanel.repaint();
+
+                } catch ( IOException ex ) {
                     ex.printStackTrace();
                 }
-                
-                if (lastImageSheet != null)
-                    imageSheetPanel.remove(lastImageSheet);
-                if (lastTilePreview != null)
-                    tilePreviewPanel.remove(lastTilePreview);
-                
-                lastImageSheet = new JLabel(new ImageIcon(tileImage));
-                lastTilePreview = new JLabel(new ImageIcon(tileImage.getSubimage(0,0,Integer.parseInt(this.tileWidth.getText()),Integer.parseInt(this.tileHeight.getText()))));
-                
-                
-                imageSheetPanel.add(lastImageSheet);
-                //imageSheetPanel.add(new TileCanvas());
-                tilePreviewPanel.add(lastTilePreview);
-                
-                this.validate();
-                this.repaint();
-                imageSheetPanel.repaint();
-                
-            }else{
-                return;
             }
         }
     }
-    
-    public void mouseDragged(MouseEvent e) {
+
+    private void setTileFromPreview( int mX, int mY, int swidth, int sheight ) {
+        int iwidth = tileImage.getWidth();
+        int iheight = tileImage.getHeight();
+        int iX = (int) Math.floor( mX / swidth ) * swidth;
+        int iY = (int) Math.floor( mY / sheight ) * sheight;
+
+        if ( ( iX + swidth ) > iwidth
+                || ( iY + sheight ) > iheight ) {
+            return;
+        }
+
+        Image img = tileImage.getSubimage( iX, iY, swidth, sheight );
+        this.tileCanvas.setImage( img );
     }
-    
-    public void mouseMoved(MouseEvent e) {
-        if (lastTilePreview != null)
-            tilePreviewPanel.remove(lastTilePreview);
-        
-        int swidth = Integer.parseInt(this.tileWidth.getText());
-        int sheight = Integer.parseInt(this.tileHeight.getText());
-        
-        lastTilePreview = new JLabel(new ImageIcon(tileImage.getSubimage((int) Math.floor(e.getX() / swidth) * swidth, (int) Math.floor(e.getY() / sheight) * sheight ,Integer.parseInt(this.tileWidth.getText()),Integer.parseInt(this.tileHeight.getText()))));
-        
-        
-        tilePreviewPanel.add(lastTilePreview);
-        
-        this.validate();
-        this.repaint();
+
+    public void mouseDragged( MouseEvent e ) {
     }
+
+    public void mouseMoved( MouseEvent e ) {
+        if ( e.getSource() == imageSheet ) {
+            if ( tileImage != null ) {
+                try {
+                    int width = Integer.parseInt( this.tileWidth.getText() );
+                    int height = Integer.parseInt( this.tileHeight.getText() );
+                    setTileFromPreview( e.getX(), e.getY(), width, height );
+                } catch ( NumberFormatException ex ) {
+                }
+            }
+        }
+    }
+
+    class ImageCanvas extends JPanel {
+
+        private Image image = null;
+
+        @Override
+        public void paint( Graphics g ) {
+            super.paint( g );
+            if ( image != null ) {
+                int x = Math.max( 0, ( this.getWidth() - image.getWidth( null ) ) / 2 );
+                int y = Math.max( 0, ( this.getHeight() - image.getHeight( null ) ) / 2 );
+                g.drawImage( image, x, y, null );
+            }
+        }
+
+        public void setImage( Image img ) {
+            this.image = img;
+            this.repaint();
+        }
+    };
 }
 
