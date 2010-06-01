8ab3f3655d0e35b87b6041e49e886d94e7074710
peterpooter <peterpooter@34d2cac7-2255-7c49-7a0e-f21af0dea8bb>
2010-06-01 07:48:33 +0000

    ImageSheetTilePreview now has a grid on the preview image.

--- Gemapis/src/MapEncodingDecoding/MetaDataManagment/ScrolledPane.java
+++ Gemapis/src/MapEncodingDecoding/MetaDataManagment/ScrolledPane.java
@@ -2,59 +2,54 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package MapEncodingDecoding.MetaDataManagment;
 
-import MapEncodingDecoding.TileDataManagment.TileCanvas;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseMotionListener;
 import javax.swing.*;
 
-class ScrolledPane
-		extends 	JFrame
-{
-	private		JScrollPane scrollPane;
+class ScrolledPane extends JFrame {
 
-	public ScrolledPane()
-	{
-		setTitle( "Scrolling Pane Application" );
-		setSize( 300, 200 );
-		setBackground( Color.gray );
+    private JScrollPane scrollPane;
 
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout( new BorderLayout() );
-		getContentPane().add( topPanel );
+    public ScrolledPane() {
+        setTitle( "Scrolling Pane Application" );
+        setSize( 300, 200 );
 
-		Icon image = new ImageIcon( "src/Resources/PokemonTileSet.png" );
-		JLabel label = new JLabel( image );
-                label.setAutoscrolls(true);
-                
-                 MouseMotionListener doScrollRectToVisible = new MouseMotionAdapter() {
-     public void mouseDragged(MouseEvent e) {
-        Rectangle r = new Rectangle(e.getX(), e.getY(), 1, 1);
-        ((JLabel)e.getSource()).scrollRectToVisible(r);
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout( new BorderLayout() );
+        getContentPane().add( topPanel );
+
+        Icon image = new ImageIcon( "src/Resources/PokemonTileSet.png" );
+        JLabel label = new JLabel( image );
+        //label.setAutoscrolls( true );
+
+        MouseMotionListener doScrollRectToVisible = new MouseMotionAdapter() {
+
+            @Override
+            public void mouseDragged( MouseEvent e ) {
+                Rectangle r = new Rectangle( e.getX(), e.getY(), 1, 1 );
+                ( (JLabel) e.getSource() ).scrollRectToVisible( r );
+            }
+        };
+        label.addMouseMotionListener( doScrollRectToVisible );
+
+
+        // Create a tabbed pane
+        scrollPane = new JScrollPane( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS );
+        //scrollPane.getViewport().add(new TileCanvas());
+        scrollPane.getViewport().add( label );
+        topPanel.add( scrollPane, BorderLayout.CENTER );
     }
- };
- label.addMouseMotionListener(doScrollRectToVisible);
 
-
-		// Create a tabbed pane
-		scrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		//scrollPane.getViewport().add(new TileCanvas());
-                scrollPane.getViewport().add(label);
-		topPanel.add( scrollPane, BorderLayout.CENTER );
-	}
-
-
-	public static void main( String args[] )
-	{
-		// Create an instance of the test application
-		ScrolledPane mainFrame	= new ScrolledPane();
-		mainFrame.setVisible( true );
-	}
+    public static void main( String args[] ) {
+        // Create an instance of the test application
+        ScrolledPane mainFrame = new ScrolledPane();
+        mainFrame.setVisible( true );
+    }
 }
 
 
--- Gemapis/src/UI/ImageSheetTilePreview.java
+++ Gemapis/src/UI/ImageSheetTilePreview.java
@@ -1,16 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * r3 : Added a grid to the preview image representing tile width and height.
+ * r2 : Class clean up to remove component recreation.
+ * r1 : Initial Revision.
  */
 package UI;
 
 import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -26,13 +30,12 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 /**
- *
  * @author jason
  */
 public class ImageSheetTilePreview extends JPanel implements
-        java.awt.event.MouseMotionListener, PropertyChangeListener {
+        java.awt.event.MouseMotionListener, PropertyChangeListener, DocumentListener {
 
-    private ImageCanvas tileCanvas;
+    private ImageJLabel tileCanvas;
     private JScrollPane imageSheetScroller;
     private JPanel imageSheetPanel;
     private JPanel belowImageSheetPanel;
@@ -41,7 +44,7 @@ public class ImageSheetTilePreview extends JPanel implements
     private JTextField tileWidth;
     private JTextField tileHeight;
     private BufferedImage tileImage = null;
-    private JLabel imageSheet = null;
+    private GridedImageJLabel imageSheet = null;
     // floor(pos/get.width)* get.width
     private String[] supportedFormats = new String[] {".jpg", ".jpeg", ".gif", ".png"};
 
@@ -49,7 +52,7 @@ public class ImageSheetTilePreview extends JPanel implements
         this.setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
 
         imageSheetPanel = new JPanel();
-        imageSheet = new JLabel();
+        imageSheet = new GridedImageJLabel();
         imageSheetPanel.add( imageSheet );
         //imageSheetPanel.addMouseListener(this);
         imageSheet.addMouseMotionListener( this );
@@ -64,7 +67,7 @@ public class ImageSheetTilePreview extends JPanel implements
 
         tilePreviewPanel = new JPanel( new BorderLayout() );
         //tilePreview = new JLabel();
-        tileCanvas = new ImageCanvas();
+        tileCanvas = new ImageJLabel();
         tilePreviewPanel.add( tileCanvas );
 
 
@@ -73,6 +76,9 @@ public class ImageSheetTilePreview extends JPanel implements
         dimensionsPanel.add( tileWidth = new JTextField( "32" ) );
         dimensionsPanel.add( new javax.swing.JLabel( "Tile height:" ) );
         dimensionsPanel.add( tileHeight = new JTextField( "60" ) );
+
+        tileWidth.getDocument().addDocumentListener( this );
+        tileHeight.getDocument().addDocumentListener( this );
 
         belowImageSheetPanel.add( dimensionsPanel );
         belowImageSheetPanel.add( tilePreviewPanel );
@@ -113,7 +119,9 @@ public class ImageSheetTilePreview extends JPanel implements
                     int height = Integer.parseInt( this.tileHeight.getText() );
                     setTileFromPreview( 0, 0, width, height );
 
+                    refreshGrid();
                     tilePreviewPanel.repaint();
+                    imageSheet.repaint();
 
                 } catch ( IOException ex ) {
                     ex.printStackTrace();
@@ -128,13 +136,32 @@ public class ImageSheetTilePreview extends JPanel implements
         int iX = (int) Math.floor( mX / swidth ) * swidth;
         int iY = (int) Math.floor( mY / sheight ) * sheight;
 
-        if ( ( iX + swidth ) > iwidth
-                || ( iY + sheight ) > iheight ) {
+        if ( ( iX + swidth ) > iwidth || ( iY + sheight ) > iheight
+                || iwidth <= 0 || iheight <= 0 ) {
+            this.tileCanvas.setImage( null );
             return;
         }
 
         Image img = tileImage.getSubimage( iX, iY, swidth, sheight );
         this.tileCanvas.setImage( img );
+    }
+
+    private void refreshGrid() {
+        int width = 0, height = 0;
+        //Two trys here: forces width and height to be tried seperatly
+        try {
+            width = Integer.parseInt( this.tileWidth.getText() );
+        } catch ( NumberFormatException nfe ) {
+        }
+        try {
+            height = Integer.parseInt( this.tileHeight.getText() );
+        } catch ( NumberFormatException nfe ) {
+        }
+        if ( width > 0 && height > 0 ) {
+            setTileFromPreview( 0, 0, width, height );
+        }
+        imageSheet.setGrid( width, height );
+        imageSheet.repaint();
     }
 
     public void mouseDragged( MouseEvent e ) {
@@ -153,7 +180,18 @@ public class ImageSheetTilePreview extends JPanel implements
         }
     }
 
-    class ImageCanvas extends JPanel {
+    public void insertUpdate( DocumentEvent e ) {
+        refreshGrid();
+    }
+
+    public void removeUpdate( DocumentEvent e ) {
+        refreshGrid();
+    }
+
+    public void changedUpdate( DocumentEvent e ) {
+    }
+
+    class ImageJLabel extends JLabel {
 
         private Image image = null;
 
@@ -171,6 +209,31 @@ public class ImageSheetTilePreview extends JPanel implements
             this.image = img;
             this.repaint();
         }
-    };
+    }
+
+    class GridedImageJLabel extends JLabel {
+
+        protected int gridWidth = 0;
+        protected int gridHeight = 0;
+
+        @Override
+        public void paint( Graphics g ) {
+            super.paint( g );
+            if ( gridWidth > 0 && gridHeight > 0 ) {
+                g.setColor( Color.yellow );
+                for ( int r = gridHeight; r < this.getHeight(); r += gridHeight ) {
+                    g.drawLine( 0, r, this.getWidth(), r );
+                }
+                for ( int c = gridWidth; c < this.getWidth(); c += gridWidth ) {
+                    g.drawLine( c, 0, c, this.getHeight() );
+                }
+            }
+        }
+
+        public void setGrid( int width, int height ) {
+            this.gridWidth = width;
+            this.gridHeight = height;
+        }
+    }
 }
 
